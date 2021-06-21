433a3739de1d177c3e348fb901e5bd051cb8d245
Aman Saini <amansaini0003@gmail.com>
2021-06-22 01:49:37 +0530

    updated js

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured", error);
+    alert("Something wrong from server side... please try again after sometime...");
+}
+
 function clickHandler() {
-    console.log("Clicked on translate");
-    console.log("input : ", txtInput.value);
-    outputDiv.innerText = "Translation will come here...";
+    var url =  getTranslationURL(txtInput.value);
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 }
 
 
