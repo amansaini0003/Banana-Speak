1f45979461227e20b97fffde05aa450acb26452f
Aman Saini <amansaini0003@gmail.com>
2021-06-30 00:51:52 +0530

    code refractor

--- app.js
+++ app.js
@@ -2,34 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var mockServerURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
-function getTranslationURL(text){
+function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text;
 }
 
-function errorHandler(error){
+function errorHandler(error) {
     console.log("Error occured", error);
     alert("Something wrong from server side... please try again after sometime...");
 }
 
 function clickHandler() {
-    var url =  getTranslationURL(txtInput.value);
+
+    var url = getTranslationURL(txtInput.value);
     fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
-    })
-    .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
 
 
 
 btnTranslate.addEventListener("click", clickHandler);
-
-
-
-
